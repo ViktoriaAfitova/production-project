f42ee585cddd77b38f8d692c494833e5f9f5dfbc
Viktoria Afitova <viktoria.afitova@alignedcode.com>
2023-06-21 16:41:03 +0300

    Add babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ["@svgr/webpack"],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["en", "ru"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
+
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
@@ -42,5 +59,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [svgLoader, fileLoader, cssLoader, typescriptLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
