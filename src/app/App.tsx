9d0eb6c9c543151520e3df560eaca9766fbac4aa
Viktoria Afitova <viktoria.afitova@alignedcode.com>
2023-07-18 11:30:50 +0300

    Add main pipeline github actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar/ui/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
-import { AppRouter } from './providers/router';
 import { useTheme } from './providers';
+import { AppRouter } from './providers/router';
 
 function App() {
   const { theme } = useTheme();
--- src/pages/HomePage/ui/HomePage.stories.tsx
+++ src/pages/HomePage/ui/HomePage.stories.tsx
@@ -1,21 +1,21 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers';
-import HomePage from './HomePage';
+// import { ComponentMeta, ComponentStory } from '@storybook/react';
+// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+// import { Theme } from 'app/providers';
+// import HomePage from './HomePage';
 
-export default {
-  title: 'pages/HomePage',
-  component: HomePage,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof HomePage>;
+// export default {
+//   title: 'pages/HomePage',
+//   component: HomePage,
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// } as ComponentMeta<typeof HomePage>;
 
-const Template: ComponentStory<typeof HomePage> = (args) => <HomePage {...args} />;
+// const Template: ComponentStory<typeof HomePage> = (args) => <HomePage {...args} />;
 
-export const Light = Template.bind({});
-Light.args = {};
+// export const Light = Template.bind({});
+// Light.args = {};
 
-export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+// export const Dark = Template.bind({});
+// Dark.args = {};
+// Dark.decorators = [ThemeDecorator(Theme.DARK)];
